6add0608c68cdb6885906592503f9e78d23a6211
Rellie Balagat <relliebalagat@gmail.com>
2022-07-28 18:31:18 +0800

    final version

---
+++ mdp/README.md
@@ -0,0 +1,12 @@
+# Example Markdown File
+This is an example Markdown file to test the preview tool
+## Features:
+* Support for links [PragProg](https://pragprog.com)
+* Support for other features
+## How to install:
+```
+go get github.com/user/program
+```
+
+
+## code not mine: it is from the book i'm reading
--- mdp/main.go
+++ mdp/main.go
@@ -1,31 +1,45 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"html/template"
+	"io"
 	"io/ioutil"
 	"os"
-	"path/filepath"
+	"os/exec"
+	"runtime"
+	"time"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
 )
 
 const (
-	header = `
-	<!DOCTYPE html>
+	defaultTemplate = `
 	<html>
 		<head>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8">
-		<title>Markdown Preview Tool</title>
+			<meta http-equiv="content-type" content="text/html; charset=utf-8">
+			<title>{{ .Title }}</title>
 		</head>
 	<body>
+		{{ .Body }}
+	</body>
+	</html>
 	`
-	footer = `
-		</body>
-	</html>`
 )
+
+type content struct {
+	Title string
+	Body  template.HTML
+}
 
 func main() {
 	// Parse flags
 	filename := flag.String("file", "", "Markdown file to preview")
+	skipPreview := flag.Bool("s", false, "Skip auto-preview")
+	tFname := flag.String("t", "", "Alternate template name")
 	flag.Parse()
 
 	// If user did not provide input file, show usage
@@ -34,22 +48,121 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := run(*filename); err != nil {
+	if err := run(*filename, *tFname, os.Stdout, *skipPreview); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func run(filename string) error {
+func run(filename, tFname string, out io.Writer, skipPreview bool) error {
 	// Read all the data from the input file and check for errors
 	input, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return err
 	}
 
-	htmlData := parseContent(input)
+	htmlData, err := parseContent(input, tFname)
+	if err != nil {
+		return err
+	}
 
-	outName := fmt.Sprintf("%s.html", filepath.Base(filename))
-	fmt.Println(outName)
+	temp, err := ioutil.TempFile("", "mdp*.html")
+	if err != nil {
+		return err
+	}
+	if err := temp.Close(); err != nil {
+		return err
+	}
+
+	outName := temp.Name()
+
+	fmt.Println(out, outName)
+
+	if err := saveHTML(outName, htmlData); err != nil {
+		return err
+	}
+
+	if skipPreview {
+		return nil
+	}
+
+	defer os.Remove(outName)
+
 	return saveHTML(outName, htmlData)
+}
+
+func parseContent(input []byte, tFname string) ([]byte, error) {
+	// Parse the markdown file through blackfriday and bluemonday
+	// to generate a valid safe HTML
+	output := blackfriday.Run(input)
+	body := bluemonday.UGCPolicy().SanitizeBytes(output)
+
+	t, err := template.New("mdp").Parse(defaultTemplate)
+	if err != nil {
+		return nil, err
+	}
+
+	if tFname != "" {
+		t, err = template.ParseFiles(tFname)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// instantiate the content type, adding the title and body
+	c := content{
+		Title: "Markdown Preview Tool",
+		Body:  template.HTML(body),
+	}
+
+	// Create a buffer of bytes to write to file
+	var buffer bytes.Buffer
+
+	if err := t.Execute(&buffer, c); err != nil {
+		return nil, err
+	}
+
+	return buffer.Bytes(), nil
+}
+
+func saveHTML(outFname string, data []byte) error {
+	// Write the bytes to the file
+	return ioutil.WriteFile(outFname, data, 0644)
+}
+
+func preview(fname string) error {
+	cName := ""
+	cParams := []string{}
+
+	// Define executable based on OS
+	switch runtime.GOOS {
+	case "linux":
+		cName = "xdg-open"
+	case "windows":
+		cName = "cmd.exe"
+		cParams = []string{"/C", "start"}
+	case "darwin":
+		cName = "open"
+	default:
+		return fmt.Errorf("OS not supported")
+	}
+
+	// Append filename to parameter slice
+	cParams = append(cParams, fname)
+
+	// Locate executable in PATH
+	cPath, err := exec.LookPath(cName)
+
+	if err != nil {
+		return err
+	}
+
+	// Open the file using default program
+	err = exec.Command(cPath, cParams...).Run()
+
+	// give the browser some time before deleting it
+	time.Sleep(2 * time.Second)
+
+	// Open the file using default program
+	return err
 }
